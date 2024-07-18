89a508f6cb89daff7cf89ee071b7763dad848de2
Yaaghnetha <yaaghnetha18@gmail.com>
2024-07-18 18:40:56 +0530

    Added renderer, camera to the scene

--- src/levell1.js
+++ src/levell1.js
@@ -1,1 +1,17 @@
 import * as THREE from "three";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+renderer.render(scene, camera);
