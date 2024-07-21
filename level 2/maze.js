f0a2ad1127c66f0a6ac64e7e5432368adfc6efac
Yaaghnetha <yaaghnetha18@gmail.com>
2024-07-22 00:05:09 +0530

    Generated a basic grid in level 2/maze.js"

---
+++ level 2/maze.js
@@ -0,0 +1,39 @@
+// Maze Gneration using Recursive Backtracking Algorithm
+
+var cols, rows;
+var w = 40; // width
+var grid = []; // 1D array
+
+function setup() {
+    createCanvas(400, 400);
+    cols = floor(width/w);
+    rows = floor(height/w);
+
+    for ( var j = 0; i < rows; j++) {
+        for( var i = 0; i < cols; i++) {
+            var cell = new Cell(i, j);
+            grid.push(cell);
+        }
+    }
+}
+
+function draw() {
+    background(51);
+    for (var i = 0; i < grid.length; i++) {
+        grid[i].show();
+    }
+}
+
+function Cell(i, j) {
+    this.i = i; // Column number
+    this.j = j; // Row number
+
+    this.show = function() {
+        var x = this.i*w;
+        var y = this.j*w;
+        stroke(255);
+        noFill();
+        rect(x, y, w, w);
+    }
+
+}
