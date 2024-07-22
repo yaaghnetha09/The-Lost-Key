e05caa5d97664de91b28e7f125f693375ce28c19
Yaaghnetha <yaaghnetha18@gmail.com>
2024-07-22 16:29:18 +0530

    Changed maze.js path from level2 folder to main and the function named Cell

---
+++ maze.js
@@ -0,0 +1,41 @@
+// Maze Gneration using Recursive Backtracking Algorithm
+
+var cols, rows;
+var w = 40; // width
+var grid = []; // 1D array
+
+function setup() {
+    createCanvas(400, 400);
+    cols = floor(width/w);
+    rows = floor(height/w);
+
+    for ( var j = 0; i < rows; j++) {
+        for( var i = 0; i < cols; i++) {
+            var cell = new Cell(i, j);
+            grid.push(cell);
+        }
+    }
+}
+
+function draw() {
+    background(51);
+    for (var i = 0; i < grid.length; i++) {
+        grid[i].show();
+    }
+}
+
+class Cell {
+    constructor(i, j) {
+        this.i = i; // Column number
+        this.j = j; // Row number
+    }
+
+    show() {
+        var x = this.i*w;
+        var y = this.j*w;
+        stroke(255);
+        noFill();
+        rect(x, y, w, w);
+    }
+
+}
