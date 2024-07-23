87c9f1d09c3d4477903490bdbbcde64c82548515
Yaaghnetha <yaaghnetha18@gmail.com>
2024-07-24 00:16:04 +0530

    Changd maze.js to remove the walls between the current and previous visited cell

--- maze.js
+++ maze.js
@@ -27,9 +27,13 @@ function draw() {
     }
 
     current.visited = true;
+    // STEP 1
     var next = current.check_neighbors();
     if (next) {
         next.visited = true;
+        // STEP 3
+        removeWalls(current, next);
+        // STEP 4
         current = next;
     }
 }
@@ -97,8 +101,29 @@ class Cell {
         }
 
         if (this.visited) {
+            noStroke(); // 
             fill(255, 0, 255, 100);
             rect(x, y, w, w);
         }
+    }
+}
+
+
+function removeWalls(a, b) {
+    var x = a.i - b.i;
+    if ( x === 1) {
+        a.walls[3] = false;
+        b.walls[1] = false;
+    } else if (x == -1) {
+        a.walls[1] = false;
+        b.walls[3] = false;
+    }
+    var y = a.j - b.j;
+    if ( y === 1) {
+        a.walls[0] = false;
+        b.walls[2] = false;
+    } else if (y == -1) {
+        a.walls[2] = false;
+        b.walls[0] = false;
     }
 }
