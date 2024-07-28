8a25c3e41aba7296956d435bcbdf835b629254b5
Yaaghnetha <yaaghnetha18@gmail.com>
2024-07-28 21:59:07 +0530

    Added light in main.js in level 2

--- level 2/main.js
+++ level 2/main.js
@@ -12,6 +12,14 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
+// renderer.setAnimationLoop( animate );
 document.body.appendChild(renderer.domElement);
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(50, 50, 50);
+scene.add(pointLight);
 
 renderer.render(scene, camera);
