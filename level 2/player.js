778e2d26ebb09701acf0f74a2cb953c6635f123e
Yaaghnetha <yaaghnetha18@gmail.com>
2024-08-02 22:21:13 +0530

    Made a dummy sphere for player for time being

--- level 2/player.js
+++ level 2/player.js
@@ -4,6 +4,13 @@ import * as THREE from "three";
 
 class Player{
     constructor(scene){
+        this.geometry = new THREE.SphereGeometry(0.3, 32, 32);
+        this.material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+        // position, initial
+        this.mesh.position.set(0, 0.5, 0);
         
+        scene.add(this.mesh);
     }
 }
