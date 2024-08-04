f348cf5cbe690d441fd2fc35b8f2c0121b90268e
Yaaghnetha <yaaghnetha18@gmail.com>
2024-08-04 12:37:19 +0530

    Added the idle state animation

--- level 2/player.js
+++ level 2/player.js
@@ -124,6 +124,44 @@ class CharacterFSM extends FiniteStateMachine {
     _Init() {
       this._AddState('idle', IdleState);
       this._AddState('walk', WalkState);
-      this._AddState('run', RunState);
     }
-  };
+};
+
+
+// comes to rest when entered
+class IdleState extends State {
+    constructor(parent) {
+      super(parent);
+    }
+  
+    get Name() {
+      return 'idle';
+    }
+  
+    Enter(prevState) {
+      const idleAction = this._parent._proxy._animations['idle'].action;
+      if (prevState) {
+        const prevAction = this._parent._proxy._animations[prevState.Name].action;
+        idleAction.time = 0.0;
+        idleAction.enabled = true;
+        idleAction.setEffectiveTimeScale(1.0);
+        idleAction.setEffectiveWeight(1.0);
+        idleAction.crossFadeFrom(prevAction, 0.5, true);
+        idleAction.play();
+      } else {
+        idleAction.play();
+      }
+    }
+  
+    Exit() {
+    }
+  
+    Update(_, input) {
+      if (input._keys.forward || input._keys.backward) {
+        this._parent.SetState('walk');
+      } else if (input._keys.space) {
+        this._parent.SetState('dance');
+      }
+    }
+};
+  
