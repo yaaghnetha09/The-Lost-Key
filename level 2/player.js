e9613c47e7e70859cb31bd4cfab7dbb8ad0f629a
Yaaghnetha <yaaghnetha18@gmail.com>
2024-08-04 13:09:14 +0530

    Almost completed player movement code

--- level 2/player.js
+++ level 2/player.js
@@ -1,28 +1,124 @@
 import * as THREE from "three";
 
-//player creation
 
-class Player{
-    constructor(scene){
-        this.geometry = new THREE.SphereGeometry(0.3, 32, 32);
-        this.material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-
-        // position, initial
-        this.mesh.position.set(0, 0.5, 0);
-        
-        scene.add(this.mesh);
+class BasicCharacterControllerProxy {
+    constructor(animations) {
+      this._animations = animations;
     }
-}
+  
+    get animations() {
+      return this._animations;
+    }
+};
+
 
 class BasicCharacterController{
-    constructor() {
+    constructor(params) {
+        this._Init(params);
+      }
+    
+      _Init(params) {
+        this._params = params;
+        this._decceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0);
+        this._acceleration = new THREE.Vector3(1, 0.25, 50.0);
+        this._velocity = new THREE.Vector3(0, 0, 0);
+    
+        this._animations = {};
         this._input = new BasicCharacterControllerInput();
-        this._stateMachine = new FiniteStateMachine();
-
+        this._stateMachine = new CharacterFSM(
+            new BasicCharacterControllerProxy(this._animations));
+    
         this.Player();
+      }
+    
+      //player creation
+      Player(){
+            this.geometry = new THREE.SphereGeometry(0.3, 32, 32);
+            this.material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+            this.mesh = new THREE.Mesh(this.geometry, this.material);
+    
+            // position, initial
+            this.mesh.position.set(0, 0.5, 0);
+            this._params.scene.add(this.mesh);
+        }
+
+
+    Update(timeInSeconds) {
+        if (!this._target) {
+          return;
+        }
+    
+        this._stateMachine.Update(timeInSeconds, this._input);
+    
+        const velocity = this._velocity;
+        const frameDecceleration = new THREE.Vector3(
+            velocity.x * this._decceleration.x,
+            velocity.y * this._decceleration.y,
+            velocity.z * this._decceleration.z
+        );
+        frameDecceleration.multiplyScalar(timeInSeconds);
+        frameDecceleration.z = Math.sign(frameDecceleration.z) * Math.min(
+            Math.abs(frameDecceleration.z), Math.abs(velocity.z));
+    
+        velocity.add(frameDecceleration);
+    
+        const controlObject = this._target;
+        const _Q = new THREE.Quaternion();
+        const _A = new THREE.Vector3();
+        const _R = controlObject.quaternion.clone();
+    
+        const acc = this._acceleration.clone();
+        if (this._input._keys.shift) {
+          acc.multiplyScalar(2.0);
+        }
+    
+        if (this._stateMachine._currentState.Name == 'dance') {
+          acc.multiplyScalar(0.0);
+        }
+    
+        if (this._input._keys.forward) {
+          velocity.z += acc.z * timeInSeconds;
+        }
+        if (this._input._keys.backward) {
+          velocity.z -= acc.z * timeInSeconds;
+        }
+        if (this._input._keys.left) {
+          _A.set(0, 1, 0);
+          _Q.setFromAxisAngle(_A, 4.0 * Math.PI * timeInSeconds * this._acceleration.y);
+          _R.multiply(_Q);
+        }
+        if (this._input._keys.right) {
+          _A.set(0, 1, 0);
+          _Q.setFromAxisAngle(_A, 4.0 * -Math.PI * timeInSeconds * this._acceleration.y);
+          _R.multiply(_Q);
+        }
+    
+        controlObject.quaternion.copy(_R);
+    
+        const oldPosition = new THREE.Vector3();
+        oldPosition.copy(controlObject.position);
+    
+        const forward = new THREE.Vector3(0, 0, 1);
+        forward.applyQuaternion(controlObject.quaternion);
+        forward.normalize();
+    
+        const sideways = new THREE.Vector3(1, 0, 0);
+        sideways.applyQuaternion(controlObject.quaternion);
+        sideways.normalize();
+    
+        sideways.multiplyScalar(velocity.x * timeInSeconds);
+        forward.multiplyScalar(velocity.z * timeInSeconds);
+    
+        controlObject.position.add(forward);
+        controlObject.position.add(sideways);
+    
+        oldPosition.copy(controlObject.position);
+    
+        if (this._mixer) {
+          this._mixer.update(timeInSeconds);
+        }
     }
-}
+};
 
 
 
@@ -127,6 +223,17 @@ class CharacterFSM extends FiniteStateMachine {
     }
 };
 
+// Define a base `State` class for your FSM
+class State {
+    constructor(parent) {
+      this._parent = parent;
+    }
+  
+    Enter() {}
+    Exit() {}
+    Update() {}
+}
+
 
 // comes to rest when entered
 class IdleState extends State {
@@ -202,6 +309,8 @@ class WalkState extends State {
       
         this._parent.SetState('idle');
     }
-  };
+}
+
+export { BasicCharacterController };
   
   
