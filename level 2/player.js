68f3f5e8f80a9f086c180f9df26987210a2038db
Yaaghnetha <yaaghnetha18@gmail.com>
2024-08-04 12:46:41 +0530

    Added the walk state update code in fsm

--- level 2/player.js
+++ level 2/player.js
@@ -162,4 +162,46 @@ class IdleState extends State {
       }
     }
 };
+
+
+//walk state
+class WalkState extends State {
+    constructor(parent) {
+      super(parent);
+    }
+  
+    get Name() {
+      return 'walk';
+    }
+  
+    Enter(prevState) {
+      const curAction = this._parent._proxy._animations['walk'].action;
+      if (prevState) {
+        const prevAction = this._parent._proxy._animations[prevState.Name].action;
+  
+        curAction.enabled = true;
+        curAction.time = 0.0;
+        curAction.setEffectiveTimeScale(1.0);
+        curAction.setEffectiveWeight(1.0);
+  
+        curAction.crossFadeFrom(prevAction, 0.5, true);
+        curAction.play();
+      } else {
+        curAction.play();
+      }
+    }
+
+    Exit() {
+    }
+  
+    Update(timeElapsed, input) {
+        if (input._keys.forward || input._keys.backward) {
+            //if the player is previously in walk state then return
+            return;
+        }
+      
+        this._parent.SetState('idle');
+    }
+  };
+  
   
